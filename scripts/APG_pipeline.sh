3ff2fc502c9f45339b77b8410660cd3944eb0341
Ammar Qaseem <ammar_alkadasi@yahoo.com>
2017-11-28 14:53:05 +0100

    update

--- scripts/APG_pipeline.sh
+++ scripts/APG_pipeline.sh
@@ -70,111 +70,105 @@ echo "PROCESSES : " $PROCESSES
 
 
 
+# copy input files with the general XML format to the folder export_step1
+cp generate_XML_files/DS/DS.xml CPI-corpora-preparing/export_step1/
+cd CPI-corpora-preparing/export_step1/
+
+# Step 1: extract all sentences from the XML file and enclose them in <s id>...</s> tags
+# kernels-howto.pdf - Appendix C, Palaga’s Learning format API Java library
+# Output: DS.xml-ptb-s.txt
+echo "step 1 : Extract all sentences from the XML file ..."
+
+java -jar preparing_parsing_input.jar -f DS.xml 
+
+
+# Step 2: build a syntactic tree parse with part-of-speech tags using the BLLIP reranking parser (also known as Charniak-Johnson parser, Charniak parser, Brown reranking parser) - https://github.com/BLLIP/bllip-parser
+# Output: DS.xml-ptb-s.txt-parsed.txt and -parsed.err
+echo "step 2 : Build a syntactic tree ..."
+cd ..
+cp export_step1/DS.xml-ptb-s.txt bllip-parser/
+cd bllip-parser/
+rm -rf DS
+mkdir DS/
+
+python bllip_parser.py -i DS.xml-ptb-s.txt -c DS -p $PROCESSES
+
+    
+# add the identifiers from the input file in each line of the current output file
+# Output: DS.xml-ptb-s.txt-parsed_modified.txt
+# ToDo: include biomedical parsing model - debug (kernels-howto.pdf)
+python merge_identifiers.py -c DS
+cd ..
+rm -rf export_step3/charniak-johnson/
+mkdir -p export_step3/charniak-johnson/
+cp bllip-parser/DS.xml-ptb-s.txt-parsed_modified.txt export_step3/charniak-johnson/
+
+
+# Step 3: merge BLLIP parser results with the XML file from step 1 in  <bracketings>...</bracketings> tags for each sentence
+# the dependency parses and the tokenization with character offsets and part-of-speech tags will also be generated within this step
+# Output: file DS.xml.inj1
+echo "step 3 : Merge BLLIP parser results with the XML file ..."
+cp export_step1/DS.xml export_step3/
+cd export_step3/
+java -jar step_3_and_5_injection_lib.jar -f DS.xml -p DS.xml-ptb-s.txt-parsed_modified.txt -o DS.xml.inj1 -i
+
+
+# Step 4: alignment of the character offsets in the original sentence with the parsed results (<bracketings>...</bracketings>)
+# Output: DS.xml.inj1-bracketing-tokens.txt.
+echo "step 4 : Alignment character offsets  ..."
+cd ..
+
+    
+cp export_step3/DS.xml.inj1 export_step4/
+cd export_step4/
+
+
+##cd CPI-corpora-preparing/export_step4/  
+
+
+# ---- This part added to solve the problem of insufficient memory(##)
+rm -rf DS
+rm -f DS.xml.inj1-bracketing-tokens.txt
+mkdir DS/
+# splitting DS.xml.inj1 into small chunk, each one contains one document
+python xmlsplitter.py -c DS
+
 #exit 1
 
-if true
-then
+# process each document individually, this process have to run sequentially to avoid the problem of insufficient memory 
+for i in `seq 1  $( ls DS -1 | wc -l ) `; 
+do
+    let "j = $i -1 "
+    java -jar step_4_BracketingTokenMapper.jar DS/$j.xml
 
-    # copy input files with the general XML format to the folder export_step1
-    cp generate_XML_files/DS/DS.xml CPI-corpora-preparing/export_step1/
-    cd CPI-corpora-preparing/export_step1/
-
-    # Step 1: extract all sentences from the XML file and enclose them in <s id>...</s> tags
-    # kernels-howto.pdf - Appendix C, Palaga’s Learning format API Java library
-    # Output: DS.xml-ptb-s.txt
-    echo "step 1 : Extract all sentences from the XML file ..."
-
-    java -jar preparing_parsing_input.jar -f DS.xml 
-
-
-    # Step 2: build a syntactic tree parse with part-of-speech tags using the BLLIP reranking parser (also known as Charniak-Johnson parser, Charniak parser, Brown reranking parser) - https://github.com/BLLIP/bllip-parser
-    # Output: DS.xml-ptb-s.txt-parsed.txt and -parsed.err
-    echo "step 2 : Build a syntactic tree ..."
-    cd ..
-    cp export_step1/DS.xml-ptb-s.txt bllip-parser/
-    cd bllip-parser/
-    rm -rf DS
-    mkdir DS/
-
-    python bllip_parser.py -i DS.xml-ptb-s.txt -c DS -p $PROCESSES
-
-    #exit 1
-        
-    # add the identifiers from the input file in each line of the current output file
-    # Output: DS.xml-ptb-s.txt-parsed_modified.txt
-    # ToDo: include biomedical parsing model - debug (kernels-howto.pdf)
-    python merge_identifiers.py -c DS
-    cd ..
-    rm -rf export_step3/charniak-johnson/
-    mkdir -p export_step3/charniak-johnson/
-    cp bllip-parser/DS.xml-ptb-s.txt-parsed_modified.txt export_step3/charniak-johnson/
-
-
-    # Step 3: merge BLLIP parser results with the XML file from step 1 in  <bracketings>...</bracketings> tags for each sentence
-    # the dependency parses and the tokenization with character offsets and part-of-speech tags will also be generated within this step
-    # Output: file DS.xml.inj1
-    echo "step 3 : Merge BLLIP parser results with the XML file ..."
-    cp export_step1/DS.xml export_step3/
-    cd export_step3/
-    java -jar step_3_and_5_injection_lib.jar -f DS.xml -p DS.xml-ptb-s.txt-parsed_modified.txt -o DS.xml.inj1 -i
-
-
-    # Step 4: alignment of the character offsets in the original sentence with the parsed results (<bracketings>...</bracketings>)
-    # Output: DS.xml.inj1-bracketing-tokens.txt.
-    echo "step 4 : Alignment character offsets  ..."
-    cd ..
-
-        
-    cp export_step3/DS.xml.inj1 export_step4/
-    cd export_step4/
-
-
-    ##cd CPI-corpora-preparing/export_step4/  
-
-
-    # ---- This part added to solve the problem of insufficient memory(##)
-    rm -rf DS
-    rm -f DS.xml.inj1-bracketing-tokens.txt
-    mkdir DS/
-    # splitting DS.xml.inj1 into small chunk, each one contains one document
-    python xmlsplitter.py -c DS
-
-    #exit 1
-
-    # process each document individually, this process have to run sequentially to avoid the problem of insufficient memory 
-    for i in `seq 1  $( ls DS -1 | wc -l ) `; 
-    do
-        let "j = $i -1 "
-        java -jar step_4_BracketingTokenMapper.jar DS/$j.xml
-
-        cat DS/$j.xml-bracketing-tokens.txt >> DS.xml.inj1-bracketing-tokens.txt
-        
-    done        
-
-    #cd CPI-corpora-preparing
-    # Step 5: injection of the aligned character offsets to the XML file DS.xml.inj1 (<bracketings>...</bracketings>)e
-    # Output: DS.xml.inj1.inj2
-    echo "step 5 : Injection of the aligned character offsets to the XML file..."
-    cd ..
-    cp export_step4/DS.xml.inj1-bracketing-tokens.txt step5_copied_from_3/
-    # ToDo: combine directories 3 and 5, because they use the same JAR file
-    cp export_step3/DS.xml.inj1 step5_copied_from_3/
-    cd step5_copied_from_3
-    java -jar step_3_and_5_injection_lib.jar -f DS.xml.inj1 -t DS.xml.inj1-bracketing-tokens.txt -o DS.xml.inj1.inj2
+    cat DS/$j.xml-bracketing-tokens.txt >> DS.xml.inj1-bracketing-tokens.txt
     
-    if [ $ExpTyp = "XX" ] && [ $ProcessTyp = "train" ]
-    then
-        
-        gzip -c -f DS.xml.inj1.inj2 > train0.txt.gz
-        echo "The training data set are pre-processed, output is \"CPI-corpora-preparing/step5_copied_from_3/train0.txt.gz\""
-        #cd ../..
-        exit
-        
-    else
-        gzip -c -f DS.xml.inj1.inj2 > test0.txt.gz
-    fi
+done        
+
+#cd CPI-corpora-preparing
+# Step 5: injection of the aligned character offsets to the XML file DS.xml.inj1 (<bracketings>...</bracketings>)e
+# Output: DS.xml.inj1.inj2
+echo "step 5 : Injection of the aligned character offsets to the XML file..."
+cd ..
+cp export_step4/DS.xml.inj1-bracketing-tokens.txt step5_copied_from_3/
+# ToDo: combine directories 3 and 5, because they use the same JAR file
+cp export_step3/DS.xml.inj1 step5_copied_from_3/
+cd step5_copied_from_3
+java -jar step_3_and_5_injection_lib.jar -f DS.xml.inj1 -t DS.xml.inj1-bracketing-tokens.txt -o DS.xml.inj1.inj2
+
+if [ $ExpTyp = "XX" ] && [ $ProcessTyp = "train" ]
+then
+    
+    gzip -c -f DS.xml.inj1.inj2 > train0.txt.gz
+    echo "The training data set are pre-processed, output is \"CPI-corpora-preparing/step5_copied_from_3/train0.txt.gz\""
+    #cd ../..
+    exit
+    
+else
+    gzip -c -f DS.xml.inj1.inj2 > test0.txt.gz
+fi
        
-#fi
+
 
 if [ $ExpTyp = "CV" ]
 then
@@ -196,7 +190,6 @@ then
 
     python rename_files_splitting.py -c DS
         
-    #python merge_splitting.py -c DS
     
     cd ..
 
@@ -217,7 +210,6 @@ then
     mkdir -p CV/
     mkdir -p CV/DS/
 
-    ##java -jar  split_AllGraphTransformer_CV.jar -f DS.xml -s splits-test-train -o CV
     python split_AllGraphTransformer_CV.py -c DS ##
 
     cd CV/DS
@@ -231,36 +223,10 @@ fi
 
 
 
-if false # To remove
-then
-    echo "copying files ..."
-    
-
-    # return to the start directory and copy all files that are needed to run the all-paths graph kernel make-experiment steps
-    cd ../../../..
-
-    mkdir -p ppi-benchmark/Corpora/splits-test-train/ #
-    mkdir -p ppi-benchmark/Corpora/splits-test-train/DS/ #
-    cp CPI-corpora-preparing/export_step6/splits-test-train/DS/* ppi-benchmark/Corpora/splits-test-train/DS/ #
-    ##cp -r CPI-corpora-preparing/export_step6/splits-test-train/DS/ ppi-benchmark/Corpora/splits-test-train/ ##
-    rm -rf ppi-benchmark/Corpora/APG/CV/corpus/DS ##
-
-    mkdir -p ppi-benchmark/Corpora/APG/
-    mkdir -p ppi-benchmark/Corpora/APG/CV/
-    mkdir -p ppi-benchmark/Corpora/APG/CV/corpus/
-    cp -r CPI-corpora-preparing/export_step6/CV/DS/ ppi-benchmark/Corpora/APG/CV/corpus/
-    mkdir -p ppi-benchmark/Corpora/Original/
-    cp CPI-corpora-preparing/export_step1/DS.xml ppi-benchmark/Corpora/Original/
-    mkdir -p ppi-benchmark/Corpora/Original-Modified/
-    cp CPI-corpora-preparing/splitting/DS.xml ppi-benchmark/Corpora/Original-Modified/ #
-
-fi
-
 echo "copying files ..."
 cd ../..
 
 
-fi
 
 mkdir -p ppi-benchmark/Corpora/Original/
 cp CPI-corpora-preparing/export_step1/DS.xml ppi-benchmark/Corpora/Original/
@@ -300,11 +266,6 @@ then
 fi
 
 
-
-
-#fi
-
-#exit 1
 
 # start the experiment
 echo "run APG pipeline ..."
